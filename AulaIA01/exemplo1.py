1a369a6593018165d839a390e6c779c96a74a046
Isabelle <86361001+icBressan@users.noreply.github.com>
2025-02-20 08:01:27 -0300

    Aula 01

---
+++ AulaIA01/exemplo1.py
@@ -0,0 +1,17 @@
+print("olá mundo!")
+
+nome = "Isabelle"
+idade = 19
+altura = 1.63
+
+casado = False
+solteiro = True
+
+print ("olá,",nome," seja bem-vinda ao python")
+print ("sua idade é", idade)
+print ("sua altura é",altura)
+
+print("status 1:",casado)
+print("status 2:",solteiro)
+
+print(nome + " sua idade é " + str(idade))
---
+++ AulaIA01/exemplo2.py
@@ -0,0 +1,12 @@
+numero1 = input("Informe um numero:")
+numero2 = input("Informe outro numero:")
+
+soma = float(numero1)+float(numero2)
+sub = float(numero1)-float(numero2)
+prod = float(numero1)*float(numero2)
+div = float(numero1)/float(numero2)
+
+print("A soma dos números é =",soma)
+print("A subtração dos números é =",sub)
+print("A multiplicação dos números é =",prod)
+print("A divisão dos números é =",div)
