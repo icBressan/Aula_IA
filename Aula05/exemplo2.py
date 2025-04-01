5de3cd300f842b8b7e399587d0833121f3cf1d0a
Isabelle <86361001+icBressan@users.noreply.github.com>
2025-04-01 20:46:21 -0300

    Update exemplo2.py

--- Aula05/exemplo2.py
+++ Aula05/exemplo2.py
@@ -1,26 +1,33 @@
 def carregaAlunosNotas():
-    alunos =[]
-    notas =[]
+    alunos = []
+    notas = []
 
     for i in range(5):
-        nome= input('Digite o nome do aluno: ')
-        nota= float(input('Digite a nota do aluno: '))
+        nome = input('Digite o nome do aluno: ')
+        nota = float(input('Digite a nota do aluno: '))
         print()
 
         alunos.append(nome)
         notas.append(nota)
 
-    return [alunos,notas]
+    return [alunos, notas]
 
 def nomeAlunoMaiorNota(matriz):
     alunos = matriz[0]
     notas = matriz[1]
 
-    for nota in notas:
-        if nota[i] > nota[]
-    
-    return[]
+    maior_nota = notas[0]  # Assume que a primeira nota é a maior
+    aluno_maior = alunos[0]  # Assume que o primeiro aluno tem a maior nota
+
+    for i in range(1, len(notas)):
+        if notas[i] > maior_nota:
+            maior_nota = notas[i]
+            aluno_maior = alunos[i]
+
+    return aluno_maior, maior_nota  
 
 print('Exemplo 2')
-print(carregaAlunosNotas())
+dados = carregaAlunosNotas()
+aluno, nota = nomeAlunoMaiorNota(dados)
 
+print(f'O aluno com a maior nota é {aluno}. Nota: {nota:.2f}.')
