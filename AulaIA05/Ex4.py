b2ef7a96b9d9b468c88f4c079db5546527f8dafe
Isabelle <86361001+icBressan@users.noreply.github.com>
2025-05-06 21:30:05 -0300

    correção funções

--- AulaIA05/Ex4.py
+++ AulaIA05/Ex4.py
@@ -1,19 +1,19 @@
-nomes = "ANA CLARA NOGUEIRA DE ARAÚJO;ANA LAURA FARIA SANTOS;ANA LAURA FERNANDES DA SILVA;BETÂNIA AMÂNCIO PEREIRA;BRENO ESTORARI DA SILVA;EDUARDO BELLO GALEGO;EDUARDO HENRIQUE DO CARMO;" "ERIKA DE BRITTO CÔNSOLO;FERNANDA GARCIA FLORIANO;GIUSEPPE FRANCESCO STRACERI IANES BAGGIO;GUSTAVO MEDEIROS DE BARROS;GUSTAVO PEREIRA GARCIA;JOAO PEDRO BILLO LUCIANO;JOAO VICTOR SALOTI ALVES;KAUA DANTAS MARTINS;KEVIN RENAN NOGUEIRA DA SILVEIRA;LAIS CUNHA THEODORO DA SILVA;LAVINIA DAL BELLO E SOUZA;LUIS OTAVIO FLORENCIO BARBOSA;LUIS OTAVIO MORAIS DE SOUZA;""LUIZA HELENA TARDELLI MARCULLI ESPINDOLA;MARIA CAROLINA SILVA GARCIA;MARIA CLARA SANTOS MANETA;MARIA FERNANDA GRESPAN BENFEITO;MARIA FERNANDA TOBIAS CHAGAS;MATHEUS CARVALHO FELISBERTO;MURILO ANTONIO BARION;NICOLY GOMES SERRANO;OCTAVIO AUGUSTO ANDREAZZI CHAVES;PEDRO VINICIUS VIEIRA VASCONCELLOS;RAFAEL CREMASCO;RAFAEL LOPES PIRES;RAQUEL MOREIRA FERNANDES CUSTODIO;""RENAN HENRIQUE TEIXEIRA;SABRYNA SATORRES BATISTINI;SOPHIA RUEDA GONÇALVES DA RITA;TAYNARA DE MELLO SIQUEIRA;TIFANI SCHIAVON MISSURA;VITORIA CIPRIANO DE JESUS;WESLEY RICARDO SILVA PEREIRA"
-notas = "9.33;8.76;6.11;1.09;2.76;2.97;9.04;3.62;3.37;1.76;8.14;2.48;3.27;9.16;9.01;1.55;2.98;3.41;8.45;5.79;0.54;2.01;6.78;1.09;4.75;0.09;7.62;0.53;8.95;6.93;1.04;2.87;7.85;6.65;6.96;9.72;9.42;0.44;6.78;7.54"
-faltas = "23;5;14;5;29;20;19;2;23;5;18;27;6;8;28;29;5;7;7;19;6;14;4;21;25;25;17;23;21;13;1;19;27;21;26;27;7;19;5;21"
+from dados import nomes, faltas
 
-lista_nomes = nomes.split(";")
-lista_notas = notas.split(";")
-lista_faltas = faltas.split(";")
+nomes = nomes.replace('\n', '').split(';')
 
-total_reprovados_falta = 0
+faltas_temp = faltas.split(';')
+faltas = []
+for falta in faltas_temp:
+    faltas.append(int(falta))
 
-for i in range(len(lista_nomes)):
-    falta = int(lista_faltas[i])
-    if falta > 20:
-        print("•", lista_nomes[i])
-        total_reprovados_falta += 1
+reprovados = []
+for i in range(len(faltas)):
+    if faltas[i] > 20:
+        reprovados.append(nomes[i])
 
-print("")
-print("### TOTAL DE REPROVADOS POR FALTA ###")
-print("Total de reprovados por falta:", total_reprovados_falta)
+print("Alunos reprovados por falta:")
+for nome in reprovados:
+    print(nome)
+print(f"Total de reprovados por falta: {len(reprovados)}")
+
